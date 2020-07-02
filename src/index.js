59dd69ba8a1e187a256d27387013379ade8109cb
Yela <anna93aleksandra@gmail.com>
2020-07-02 22:07:44 +0100

    styled version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+
 const container = document.querySelector('.container');
 const taskNameInput = document.querySelector('#taskName');
 const saveButton = document.querySelector('#save');
-const resetButton = document.querySelector('#reset')
+const resetButton = document.querySelector('#reset');
+const addButton = document.querySelector('#addNew');
+const newTaskScreen = document.querySelector('#newTask');
 
+let tasks = [ 
+    {name: "Taskname",
+    description: "Task description",
+    date: "Due Date: 24 Jun" }
+];
 
+addButton.addEventListener('click', showAddNewTask);
 
-//const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-let tasks = [];
+function showAddNewTask() {
+    
+    newTaskScreen.classList.remove('hidden');
+    newTaskScreen.classList.add('show');
+}
+
+function hideAddNewTask() {
+    newTaskScreen.classList.remove('show');
+    newTaskScreen.classList.add('hidden');
+}
 
 saveButton.addEventListener('click', addNewTaskToTheArray);
 
@@ -17,26 +34,41 @@ function Todo(name, description, date) {
 
 }
 
-function addNewTaskToTheArray() {
+function addNewTaskToTheArray(e) {
+    e.preventDefault();
+    
     let taskname = document.querySelector('#taskName').value;
     let taskDescription = document.querySelector('#description').value;
     let taskDate = document.querySelector('input[type="date"]').value;
-    let array = "";
 
 
-    
 
     let newElement = new Todo(taskname, taskDescription, getDate(taskDate))
 
+  
     tasks = [];
     tasks.push(newElement);
+    hideAddNewTask();
+    displayTasks();
+
+    
 
     document.querySelector('#taskName').value = "";
     document.querySelector('#description').value = "";
     document.querySelector('input[type="date"]').value = "";
 
-    displayTasks();
+
 }
+/*
+function sendToFirebase(taskname, taskDescription) {
+    let newItem = firebaseReference.push();
+    newItem.addNewTaskToTheArray({
+        name: taskname,
+        description: taskDescription,
+
+    });
+}
+*/
 
 function displayTasks() {
    
@@ -46,16 +78,19 @@ function displayTasks() {
         taskElement.innerText = tasks[i].name;
 
         const description = document.createElement('p');
+        description.classList.add('smalltext');
         description.innerText = tasks[i].description;
 
         let date = document.createElement('p');
-        date.innerText = tasks[i].date;
+        date.classList.add('smalltext');
+        date.innerText = "Due date: " + tasks[i].date;
         
         taskElement.appendChild(description);
         taskElement.appendChild(date);
         container.appendChild(taskElement);
     }
 }
+
 
 function getDate(taskDate) {
     let array = taskDate.split('-');
@@ -91,16 +126,37 @@ function getDate(taskDate) {
     }
 
     array = array.join(" ")
-    
-    
 
     return array
 }
 
+function addTaskManuallyForStyling() {
+    const taskElement = document.createElement('li');
+    taskElement.classList.add('task');
+    taskElement.innerText = "Pay Council Tax";
+
+    const description = document.createElement('p');
+    description.classList.add('smalltext');
+    description.innerText = "Council Tax 139 pounds";
+
+
+    let date = document.createElement('p');
+    date.classList.add('smalltext');
+    date.innerText = "Due Date: 5 Jul";
+    
+    taskElement.appendChild(description);
+    taskElement.appendChild(date);
+    container.appendChild(taskElement);
+
+}
+
+addTaskManuallyForStyling();
 
 
 
 
+
+  //sendToFirebase(taskname, taskDescription);
 
 
 
